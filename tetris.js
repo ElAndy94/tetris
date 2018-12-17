7e8738971e48bb9d583e3bdf0218a26396a060e0
Andrew <andrewpeliza@hotmail.com>
2018-12-17 16:15:45 +0000

    Game area + now the tetris stacks, rotation next.

--- tetris.js
+++ tetris.js
@@ -9,10 +9,32 @@ const matrix = [
     [0, 1, 0],
 ];
 
+collide = (gameArea, player) => {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && 
+                (gameArea[y + o.y] && gameArea[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+createMatrix = (w, h) => {
+    const matrix = [];
+    while(h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 draw = () => {
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.clientWidth, canvas.height);
 
+    drawMaterix(gameArea, {x: 0, y: 0});
     drawMaterix(player.matrix, player.pos);
 }
 
@@ -29,6 +51,33 @@ drawMaterix = (matrix, offset) => {
             }
         });
     });
+}
+
+merge = (gameArea, player) => {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                gameArea[y + player.pos.y][x + player.pos.x] = value;
+            }
+        })
+    })
+}
+
+playerDrop = () => {
+    player.pos.y ++;
+    if (collide(gameArea , player)) {
+        player.pos.y --;
+        merge(gameArea, player);
+        player.pos.y = 0;
+    }
+    dropCounter = 0;
+}
+
+playerMove = (direction) => {
+    player.pos.x += direction;
+    if (collide(gameAream, player)) {
+        player.pos.y -= direction;
+    }
 }
 
 let dropCounter = 0;
@@ -49,10 +98,22 @@ update = (time = 0) => {
     requestAnimationFrame(update);
 }
 
+const gameArea = createMatrix(12, 20);
+
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix
 }
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 37) {
+        playerMove(-1);
+    } else if (event.keyCode === 39) {
+        playerMove(1);
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+})
 
 update();
 
